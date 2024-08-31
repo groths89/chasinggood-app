5354b10626f9d73295ec793c2d24a8a9199e579e
Gregory Paul Rothstein <g.p.rothstein@gmail.com>
2024-08-31 18:02:11 -0400

    Fix for $data

--- resources/views/emails/thank_you_nominator.blade.php
+++ resources/views/emails/thank_you_nominator.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-white p-4 rounded-lg shadow-md">
     <h2 class="text-2xl font-bold text-gray-800 mb-4">Thank You for Submitting a Good Deed!</h2>
-    <p class="text-gray-600 mb-2">Thank you {{ first_name . " " . last_name }} for nominating {{ nominee_name }} for the annual Chasing Good Award.</p>
+    <p class="text-gray-600 mb-2">Thank you {{ $data['first_name'] . " " . $data['last_name'] }} for nominating {{ $data['nominee_name'] }} for the annual Chasing Good Award.</p>
     <p class="text-gray-600 mb-2">Your kindness and generosity in sharing this heartwarming story means a lot to us.</p>
     <p class="text-gray-600 mb-2">We will be in touch in December if we need further information and/or if you win!</p>
     <p class="text-gray-600 mb-2">Sincerely,</p>
